4bc67efc2852fbc9a1516c7ac9200c7c114237bf
SuprajaBiradar <suprajabiradar@gmail.com>
2025-06-18 10:38:59 +0530

    website/about

---
+++ Mission.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Mission = () => {
+  return (
+    <div>
+      <h3>Our Mission</h3>
+      <p>To build high-quality software that solves real problems.</p>
+    </div>
+  );
+};
+
+export default Mission;
---
+++ Team.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Team = () => {
+  return (
+    <div>
+      <h3>Meet the Team</h3>
+      <p>We are a group of passionate developers.</p>
+    </div>
+  );
+};
+
+export default Team;
