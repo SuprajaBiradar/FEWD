51b106b2b0d48bed57e4ee170752a84b9be7eb2e
SuprajaBiradar <suprajabiradar@gmail.com>
2025-06-18 10:34:51 +0530

    Pages

---
+++ About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Team from '../components/website/About/Team';
+import Mission from '../components/website/About/Mission';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <Team />
+      <Mission />
+    </div>
+  );
+};
+
+export default About;
---
+++ Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../components/website/Home/Banner';
+import Features from '../components/website/Home/Features';
+import Testimonials from '../components/website/Home/Testimonials';
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Features />
+      <Testimonials />
+    </div>
+  );
+};
+
+export default Home;
